b622b76595067120213159246c8cb0092e3ca5ce
despervita <118569756+despervita@users.noreply.github.com>
2022-11-18 18:28:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Marceau',         # How you named your package folder (MyLib)
   packages = ['Marceau'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "Panjer's Algorithm in Python",   # Give a short description about your library
   author = 'Rayane Vigneron',                   # Type in your name
