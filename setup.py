ec801b36ead1db1929dc8ce49803b0ef4b58bb50
despervita <118569756+despervita@users.noreply.github.com>
2022-11-18 18:26:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Rayane Vigneron',                   # Type in your name
   author_email = 'rayanevigneron@yahoo.fr',      # Type in your E-Mail
   url = 'https://github.com/despervita/Marceau',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/despervita/Marceau/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/despervita/Marceau/archive/v_02.tar.gz',    # I explain this later on
   keywords = ['Panjer', 'Python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
