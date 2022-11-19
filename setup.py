0d2fef894709261ff89fdaa9096b26369887a833
despervita <118569756+despervita@users.noreply.github.com>
2022-11-18 23:45:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Marceau',         # How you named your package folder (MyLib)
   packages = ['Marceau'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.41',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "Panjer's Algorithm in Python",   # Give a short description about your library
   author = 'Rayane Vigneron',                   # Type in your name
   author_email = 'rayanevigneron@yahoo.fr',      # Type in your E-Mail
   url = 'https://github.com/despervita/Marceau',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/despervita/Marceau/archive/refs/tags/v_04.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/despervita/Marceau/archive/refs/tags/v_041.tar.gz',    # I explain this later on
   keywords = ['Panjer', 'Python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
